912a675ac197c67a062c6ada41373def965056f4
Kristian Carr <kristian.carr@sky.com>
2024-08-08 16:41:18 +0100

    finished implementation of assignment 8

--- assignment-8/write-cities.go
+++ assignment-8/write-cities.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
+	const citiesFileName string = "myfavouritecities.txt"
 	var cityList = [7]string{"Abu Dhabi", "London", "Washington D.C.", "Montevideo", "Vatican City", "Caracas", "Hanoi"}
 
-	WriteFileToDisk(cityList[:])
+	WriteFileToDisk(cityList[:], citiesFileName)
+
+	OpenFileFromDisk(citiesFileName)
 }
 
-func WriteFileToDisk(contents []string) {
-	file, err := os.Create("myfavouritecities.txt")
+func OpenFileFromDisk(fileName string) {
+	contentBytes, err := os.ReadFile(fileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	stringContent := string(contentBytes)
+	fmt.Println(stringContent)
+}
+
+func WriteFileToDisk(contents []string, fileName string) {
+	file, err := os.Create(fileName)
 
 	if err != nil {
 		log.Fatal(err)
