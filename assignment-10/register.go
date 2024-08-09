e0af2072fb9f3ba57c416cb2afd1232320569af7
Kristian Carr <kristian.carr@sky.com>
2024-08-09 15:47:12 +0100

    Implemented assignment 10

---
+++ assignment-10/register.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Pupil struct {
+	fullName    FullName
+	dateOfBirth string
+	age         int
+}
+
+type FullName struct {
+	firstName  string
+	middleName string
+	lastName   string
+}
+
+var pupils = []Pupil{
+	{
+		fullName: FullName{
+			firstName:  "Obi",
+			middleName: "Wan",
+			lastName:   "Kenobi",
+		},
+		dateOfBirth: "07/04/2015",
+		age:         9,
+	},
+	{
+		fullName: FullName{
+			firstName:  "Qui",
+			middleName: "Gon",
+			lastName:   "Jin",
+		},
+		dateOfBirth: "18/07/2015",
+		age:         9,
+	},
+	{
+		fullName: FullName{
+			firstName:  "Luke",
+			middleName: "",
+			lastName:   "Skywalker",
+		},
+		dateOfBirth: "20/11/2014",
+		age:         10,
+	},
+	{
+		fullName: FullName{
+			firstName:  "Leia",
+			middleName: "",
+			lastName:   "Skywalker",
+		},
+		dateOfBirth: "20/11/2014",
+		age:         10,
+	},
+	{
+		fullName: FullName{
+			firstName:  "Llando",
+			middleName: "",
+			lastName:   "Calrissian",
+		},
+		dateOfBirth: "15/10/2014",
+		age:         10,
+	},
+	{
+		fullName: FullName{
+			firstName:  "Jean",
+			middleName: "Luc",
+			lastName:   "Picard",
+		},
+		dateOfBirth: "02/03/2015",
+		age:         10,
+	},
+	{
+		fullName: FullName{
+			firstName:  "Geordie",
+			middleName: "La",
+			lastName:   "Ford",
+		},
+		dateOfBirth: "06/07/2015",
+		age:         10,
+	},
+	{
+		fullName: FullName{
+			firstName:  "Kira",
+			middleName: "",
+			lastName:   "Nerys",
+		},
+		dateOfBirth: "22/04/2015",
+		age:         10,
+	},
+	{
+		fullName: FullName{
+			firstName:  "Jadzia",
+			middleName: "",
+			lastName:   "Dax",
+		},
+		dateOfBirth: "10/09/2015",
+		age:         10,
+	},
+	{
+		fullName: FullName{
+			firstName:  "Miles",
+			middleName: "",
+			lastName:   "O'Brien",
+		},
+		dateOfBirth: "28/10/2015",
+		age:         10,
+	},
+}
+
+func main() {
+	printHeader()
+	for _, pupil := range pupils {
+		fmt.Println(pupil)
+	}
+	printBar()
+}
+
+func printHeader() {
+	printBar()
+	fmt.Println("| First Name      | Middle Name     | Last Name       | Date of Birth   | Age             |")
+	printBar()
+}
+
+func printBar() {
+	fmt.Println("|-----------------|-----------------|-----------------|-----------------|-----------------|")
+}
+
+func (p Pupil) String() string {
+	return fmt.Sprintf("| %-15s | %-15s | %-15s | %-15s | %-15d |", p.fullName.firstName, p.fullName.middleName, p.fullName.lastName, p.dateOfBirth, p.age)
+}
